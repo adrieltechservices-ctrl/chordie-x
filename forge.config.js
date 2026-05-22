1543b7a3494861c3b98641de0d373b7bf1c65616
adrieltechservices-ctrl <adrieltechservices@gmail.com>
2026-05-21 19:21:54 -0500

    Compile and Build Native Software Installers

---
+++ forge.config.js
@@ -0,0 +1,13 @@
+{
+  "build": {
+    "appId": "com.chordie-x.app",
+    "productName": "Chordie X",
+    "mac": {
+      "target": ["dmg", "zip"],
+      "category": "public.app-category.music"
+    },
+    "win": {
+      "target": ["nsis", "portable"]
+    }
+  }
+}
